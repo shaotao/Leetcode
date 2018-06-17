bb89d0b1915eaae0ee2276f42399a3e8b7941344
Tao SHAO <taoshao@dbs.com>
2018-06-17 10:21:02 +0800

    [595] Big Countries completed.

--- database/nth_highest_salary/nth_highest_salary.sql
+++
@@ -1,9 +0,0 @@
-CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
-BEGIN
-  DECLARE o INT; 
-  SET o = N-1;
-  RETURN (
-      # Write your MySQL query statement below.
-      select ifnull( (select distinct Salary from Employee order by Salary desc limit 1 offset o), null)
-  );
-END
--- database/rising_temperature/rising_temperature.sql
+++
@@ -1,1 +0,0 @@
-select t1.Id from Weather as t1, Weather as t2 where to_days(t1.Date) = to_days(t2.Date)+1 and t1.Temperature > t2.Temperature;
